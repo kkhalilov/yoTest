4285f06423cac3e804330c6edae6e10686b46791
khalil <khalil.ov@yandex.ru>
2018-03-30 20:26:45 +0300

    add slick and price col

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,18 @@
 console.log('\'Allo \'Allo!');
+
+$('.slider-for').slick({
+    speed: 800,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    asNavFor: '.slider-for',
+    centerMode: true,
+    focusOnSelect: true
+});
